c9434d3cefefe8c6cab70830c70c10eb32736a81
srijachatterjee19 <srijachatterjee9218@gmail.com>
2026-04-10 16:34:35 +0530

    add api monitoring

--- backend-service/index.js
+++ backend-service/index.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const fs = require("fs");
+const client = require("prom-client");
 
 const app = express();
+
+const client = require("prom-client");
+
+// custom counter
+const requestCounter = new client.Counter({
+    name: "http_requests_total",
+    help: "Total HTTP requests",
+  });
+
+  // middleware
+app.use((req, res, next) => {
+    requestCounter.inc();
+    next();
+  });
+  
 
 // Load songs
 const songs = JSON.parse(fs.readFileSync("songs.json", "utf-8"));
@@ -15,6 +31,11 @@ app.get("/songs", (req, res) => {
   res.json(songs);
 });
 
+app.get("/metrics", async (req, res) => {
+    res.set("Content-Type", client.register.contentType);
+    res.end(await client.register.metrics());
+  });
+  
 app.listen(5000, () => {
   console.log("Node service running on 5000");
 });
--- flask-service/app.py
+++ flask-service/app.py
@@ -1,10 +1,21 @@
 from flask import Flask, jsonify
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
 
+REQUEST_COUNT = Counter('http_requests_total', 'Total Requests')
+
+@app.before_request
+def before():
+    REQUEST_COUNT.inc()
+
 @app.route("/")
 def home():
-    return jsonify({"message": "Hello from Flask 🐍"})
+    return jsonify({"message": "Hello from Flask"})
+
+@app.route("/metrics")
+def metrics():
+    return generate_latest()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
